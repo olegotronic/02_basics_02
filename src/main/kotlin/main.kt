36b9590d2398fd01f41d9a466f4697dce5b1e94a
Oleg Krugov <olegotron@gmail.com>
2022-03-08 17:32:49 +0300

    Fix: "11 человеку"

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,8 @@
 fun main() {
 
-    val likes = 1031
+    val likes = 1011
 
-    val peoplesPresentation = if (likes % 10 == 1) "человеку" else "людям"
+    val peoplesPresentation = if (likes % 10 == 1 && likes % 100 != 11) "человеку" else "людям"
 
     println("Понравилось $likes $peoplesPresentation")
 
